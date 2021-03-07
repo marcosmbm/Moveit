f712ed1f57ee2efce96ee62ea0f38f8d580ab206
Marcos Barbosa <marmbmti@gmail.com>
2021-03-07 00:32:34 -0300

    Moveit

--- src/context/CountDownContext.tsx
+++ src/context/CountDownContext.tsx
@@ -25,7 +25,7 @@ export function CountDownProvider({children} : CountDownProviderProps){
 
 
 
-    const [time,setTime] = useState(0.1 * 60);
+    const [time,setTime] = useState(25 * 60);
     const [isActive,setIsActive] = useState(false);
     const [hasFinished,setHasFinished] = useState(false);
 
